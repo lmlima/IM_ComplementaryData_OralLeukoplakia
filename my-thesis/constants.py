aa2705e6202312373b42854087b0a134625efe95
lmlima <lmlima@users.noreply.github.com>
2023-08-09 14:13:46 -0300

    Update constants.py

--- my-thesis/constants.py
+++ my-thesis/constants.py
@@ -9,10 +9,6 @@ Please, use this file to set the constants that will be used to load important l
 # The path to Raug. You may find it here: https://github.com/paaatcha/raug
 RAUG_PATH = "/dataset/code/SkinLesion/raug"
 
-# The path to TOPSIS (if you're going to use it). You may find it here:
-# https://github.com/paaatcha/decision-making/tree/master/TOPSIS
-TOPSIS_PATH = "/home/patcha/Codes/decision-making/TOPSIS"
-
 DATA_PATH = "/dataset/data"
 
 TELEGRAM_TOKEN = ""
